9a610fb1ea8b01069a5997aac360aa748ba5786e
joebrenny <brejosj@dunwoody.edu>
2020-02-06 20:47:42 -0600

    added Joe Burrow and the class with vars

---
+++ project1/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    class Player
+    {
+        //data encapulation create class with the vars for each player 
+        public string Name;
+        public string College;
+        public double Salary;
+        public string Position;
+        public int rank;
+
+ 
+
+            //getter and setter methods
+       public void setName(string name)
+        {
+            //this is a key word that is used for 
+            this.Name = name;
+            //could also use name = aname
+        }
+        //this is the get method 
+        public string getName()
+        {
+            return Name;
+        }
+         public void setCollege(string college)
+        {
+            this.College = college;
+        }
+        public string getCollege()
+        {
+            return College;
+        }
+        public void setSalary(double salary)
+        {
+            this.Salary = salary;
+        }
+        public double getSalary()
+        {
+            return Salary;
+        }
+        public void setPosition(string position)
+        {
+            this.Position = position;
+        }
+        public string getPosition()
+        {
+            return Position;
+        }
+    }// end player class
+}
--- project1/Program.cs
+++ project1/Program.cs
@@ -3,10 +3,25 @@
 namespace project1
 {
     class Program
+
+        //need to create a class so i can make the objects of players
+        //use a bool value once the a player is selected so they user can use it twice
     {
         static void Main(string[] args)
+
         {
             Console.WriteLine("checking commit to repo");
+            //creating the object for Joe Burrow 
+            Player QBone = new Player();
+            QBone.setName("Joe Burrow");
+            QBone.setCollege("LSU");
+            QBone.setSalary(26000000);
+            QBone.setPosition("Quarter Back");
+
+            Console.WriteLine($"Name:{ QBone.Name}");
+
+            //christian.Greeting();
+            //christian.bio();
         }
-    }
+    }// end of the class program 
 }
